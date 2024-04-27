ffb60c3fbb064ef9b664ad68500b4a7a27472eed
Travis <travis@mooremediallc.com>
2024-04-26 21:53:19 -0700

    testing webhook

--- components/marketing/sections/hero1-section.tsx
+++ components/marketing/sections/hero1-section.tsx
@@ -9,7 +9,7 @@ export function HeroFullSection() {
         <div className="container">
           <div className="text-center space-y-5 max-w-5xl mx-auto py-10">
             <h3 className={`${caveat.className} text-3xl sm:text-5xl`}>Struggling to ship your online startup?</h3>
-            <h1 className="text-5xl sm:text-6xl font-black">Launch Your SaaS Startup in Minutes, Not Months!</h1>
+            <h1 className="text-5xl sm:text-6xl font-black">Launch Your SaaS Startup in Minutes, Not Months!!!</h1>
             <p className="text-xl">Skip the tedious, time-consuming boilerplate code and focus on launching your idea to achieve product-market fit blazingly fast!</p>
             <Button size="lg" asChild>
             <Link href="#pricing-section">
