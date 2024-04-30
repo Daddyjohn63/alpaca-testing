956cdbbab10eadadeb7d9b9d27e41d67bea85579
Travis <travis@mooremediallc.com>
2024-04-30 09:36:51 -0700

    stylize blog details content css

--- scripts/database.ts
+++ scripts/database.ts
@@ -10,41 +10,25 @@ export const addSlug = async () => {
     }
   })
 
-  console.log("Print Database")
-
-
-
   data.forEach(async (post) => {
 
-    
-    const existingCount = data.filter((data) => post.title === data.title)
-
-    console.log(typeof(existingCount.length))
-
-      
-    if(existingCount.length > 1) {
-
-      const extentionString = `-${existingCount.length}`
-      const slug = changeCase.kebabCase(post.title) + extentionString
-      console.log(slug)
-
-    }
-
-      const slug = changeCase.kebabCase(post.title)
-    
-
-
+    const slug = changeCase.kebabCase(post.title)
+   
     console.log(slug)
+
+    await db.post.update({
+      where: {
+        id: post.id
+      },
+      data: {
+        slug: slug
+      }
+    })
     
 
-    // await db.post.update({
-    //   where: {
-    //     id: post.id
-    //   },
-    //   data: {
-    //     slug: slug
-    //   }
-    // })
+
+    
+
 
 
   })
