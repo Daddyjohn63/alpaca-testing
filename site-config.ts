13ce6303f28dfdf0634a118db49921c18beae26b
Travis <travis@mooremediallc.com>
2024-05-25 18:58:02 -0700

    added under construction tag

--- site-config.ts
+++ site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   appName: "Alpacastack",
   appDescription: "One site to rule them all",
-  isUnderConstruction: false, //Options: true, false - Ads an under construction page to all but /admin route.  
+  isUnderConstruction: true, //Options: true, false - Ads an under construction page to all but /admin route.  
   isBlogPublic: true, //Options: true, false - Redirects /blog route to 404. Be sure to remove blog route from constants if false.
   themeColor: 'dark', //Options: light, dark - Colors adjusted in global.css
   routes: {
