e6bd552b05e5629265f360e98f695a13dd3a8dcb
Travis <travis@mooremediallc.com>
2024-05-27 20:40:45 -0700

    fixing sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 //Reference: https://www.npmjs.com/package/next-sitemap#create-config-file
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_PRIMARY_DOMAIN || "https://alpacastack.com",
+  siteUrl: "https://staging.alpacastack.com",
   generateRobotsTxt: true,
   exclude: ["/dashboard", "/admin", "/twitter-image.*", "/opengraph-image.*", "/icon.*"],
 };
