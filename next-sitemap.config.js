1d52dbced73e3b752e4484977a112358d25d9b07
Travis <travis@mooremediallc.com>
2024-05-27 20:59:15 -0700

    fixing sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 //Reference: https://www.npmjs.com/package/next-sitemap#create-config-file
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://staging.alpacastack.com",
+  siteUrl: "http://localhost:3000",
   generateRobotsTxt: true,
-  exclude: ["/dashboard", "/admin", "/twitter-image.*", "/opengraph-image.*", "/icon.*"],
+  exclude: ["/api/auth/*", "/dashboard/*", "/admin/*", "/twitter-image.*", "/opengraph-image.*", "/icon.*"],
 };
 
