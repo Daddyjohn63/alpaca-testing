146d4fdca1b9b13a4282d7033259ebfacc1fb2dd
Travis <travis@mooremediallc.com>
2024-05-29 21:36:11 -0700

    finish

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_PRIMARY_DOMAIN || "https://staging.alpacastack.com",
-  generateRobotsTxt: true,
-  generateIndexSitemap: false, //Node module currently not working so disabling it for now. 
-  exclude: ["/api/*", "/dashboard/*", "/admin/*", "/twitter-image.*", "/opengraph-image.*", "/icon.*"],
-};
-
