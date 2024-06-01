49599e0ca509fa3116ff0cfc020aaa6874e12a0d
Travis <travis@mooremediallc.com>
2024-05-31 18:12:14 -0700

    under construction in main

--- site-config.ts
+++ site-config.ts
@@ -2,8 +2,8 @@ export const siteConfig = {
   appName: "Alpacastack",
   appDescription: "Launch your SaaS in minutes, not months!",
   keywords: ['Next.js', 'React', 'JavaScript'],
-  primaryDomain: 'staging.alpacastack.com',
-  isUnderConstruction: false, //Options: true, false - Ads an under construction page to all but /admin route.  
+  primaryDomain: 'alpacastack.com',
+  isUnderConstruction: true, //Options: true, false - Ads an under construction page to all but /admin route.  
   isBlogPublic: false, //Options: true, false - Redirects /blog route to 404. Be sure to remove blog route from constants/nav-routes.ts if false.
   themeColor: 'dark', //Options: light, dark - Colors adjusted in global.css
   email: {
