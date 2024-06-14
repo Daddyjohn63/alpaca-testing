924b8082ccbbf7705b331598b3867bfbae0e5228
Travis <travis@mooremediallc.com>
2024-06-13 18:01:33 -0700

    fixed nav menu

--- components/mobile-nav-btn.tsx
+++ components/mobile-nav-btn.tsx
@@ -16,8 +16,6 @@ import { NavItems } from "@/types";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { usePathname } from "next/navigation";
 
-
-
 export function MobileNavBtn({navItemsArray}: {navItemsArray: NavItems[]}) {
 
   const user = useCurrentUser()
@@ -49,22 +47,22 @@ export function MobileNavBtn({navItemsArray}: {navItemsArray: NavItems[]}) {
                 return (
                   <li key={i}>
                     <SheetClose asChild>
-                      <>
-                        <Link href={item.href} className={`${pathname === item.href ? "bg-muted" : ""} hover:bg-primary hover:text-primary-foreground p-3 rounded-sm flex gap-2 items-center`}>
-                          {item.icon && <Icon />} {item.text}
-                        </Link>
-                        <ul className="pl-4">
-                          {item.subMenu && item.subMenu.map((subItem, j) => {
-                            return (
-                              <li key={j}>
-                                <Link href={subItem.href} className={`${pathname === subItem.href ? "bg-muted" : ""} hover:bg-primary hover:text-primary-foreground text-foreground/60 py-2 px-3 rounded-sm flex gap-2 items-center`}>
-                                  {subItem.title}</Link>
-                              </li>
-                            )
-                          })}
-                        </ul>
-                      </> 
+                      <Link href={item.href} className={`${pathname === item.href ? "bg-muted" : ""} hover:bg-primary hover:text-primary-foreground p-3 rounded-sm flex gap-2 items-center`}>
+                        {item.icon && <Icon />} {item.text}
+                      </Link>
                     </SheetClose>
+                    <ul className="pl-4">
+                      {item.subMenu && item.subMenu.map((subItem, j) => {
+                        return (
+                          <li key={j}>
+                            <SheetClose asChild>
+                              <Link href={subItem.href} className={`${pathname === subItem.href ? "bg-muted" : ""} hover:bg-primary hover:text-primary-foreground text-foreground/60 py-2 px-3 rounded-sm flex gap-2 items-center`}>
+                                {subItem.title}</Link>
+                            </SheetClose>
+                          </li>
+                        )
+                      })}
+                    </ul>
                   </li>
                 )
               })}
